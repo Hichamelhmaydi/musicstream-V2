2d4eee54e89756eab96c0e109344cfcb1eb5f7b1
Hichamelhmaydi <ct1594065@gmail.com>
2026-01-27 17:25:58 +0100

    add frond end with angular

---
+++ musicstream-frontend-tailwind/src/app/components/audio-controls.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-audio-controls',
+  imports: [],
+  templateUrl: './audio-controls.html',
+  styleUrl: './audio-controls.css',
+})
+export class AudioControls {
+
+}
---
+++ musicstream-frontend-tailwind/src/app/models/track.model.ts
@@ -0,0 +1,19 @@
+
+export interface Track {
+  id?: number;
+  title: string;
+  artist: string;
+  description: string;
+  category: string;
+  duration: number;
+  audioUrl: string;
+  coverUrl?: string;
+  addedDate?: Date;
+}
+
+export interface TrackFilters {
+  search: string;
+  category: string;
+  sortBy: 'title' | 'artist' | 'addedDate' | 'duration';
+  sortOrder: 'asc' | 'desc';
+}
