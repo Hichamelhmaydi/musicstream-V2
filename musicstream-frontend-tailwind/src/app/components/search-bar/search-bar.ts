b28da2bb02a7bddfd89f15776a65d0198eb78da8
Hichamelhmaydi <ct1594065@gmail.com>
2026-01-29 14:51:56 +0100

    implement search bar component with filter management and sorting functionality

--- musicstream-frontend-tailwind/src/app/components/search-bar/search-bar.ts
+++ musicstream-frontend-tailwind/src/app/components/search-bar/search-bar.ts
@@ -10,4 +10,45 @@ import { TrackFilters } from '../../models/track.model';
   templateUrl: './search-bar.html',
   styles: []
 })
-export class SearchBarComponent {}
+export class SearchBarComponent implements OnInit {
+  @Output() filtersChange = new EventEmitter<TrackFilters>();
+  
+  filters: TrackFilters = {
+    search: '',
+    category: '',
+    sortBy: 'title',
+    sortOrder: 'asc'
+  };
+  
+  categories: string[] = [
+    'Pop', 'Rock', 'Hip Hop', 'Jazz', 'Classical', 
+    'Electronic', 'R&B', 'Country', 'Reggae', 'Metal'
+  ];
+
+  ngOnInit(): void {
+    this.onFiltersChange();
+  }
+
+  onFiltersChange(): void {
+    this.filtersChange.emit({ ...this.filters });
+  }
+
+  toggleSortOrder(): void {
+    this.filters.sortOrder = this.filters.sortOrder === 'asc' ? 'desc' : 'asc';
+    this.onFiltersChange();
+  }
+
+  clearSearch(): void {
+    this.filters.search = '';
+    this.onFiltersChange();
+  }
+
+  clearCategory(): void {
+    this.filters.category = '';
+    this.onFiltersChange();
+  }
+
+  hasActiveFilters(): boolean {
+    return !!this.filters.search || !!this.filters.category;
+  }
+}
