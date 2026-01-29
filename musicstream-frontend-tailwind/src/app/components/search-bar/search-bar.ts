a26a336dcbeec19d3d3a928cc47835d10b49cd0f
Hichamelhmaydi <ct1594065@gmail.com>
2026-01-29 10:59:08 +0100

    add serach bar page

--- musicstream-frontend-tailwind/src/app/components/search-bar/search-bar.ts
+++ musicstream-frontend-tailwind/src/app/components/search-bar/search-bar.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { TrackFilters } from '../../models/track.model';
 
 @Component({
   selector: 'app-search-bar',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './search-bar.html',
-  styleUrl: './search-bar.css',
+  styles: []
 })
-export class SearchBar {
-
-}
+export class SearchBarComponent {}
